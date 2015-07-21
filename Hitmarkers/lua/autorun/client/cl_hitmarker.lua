5fd247d5b61a05d1c119b6319ed11efe83ae03e7
ThatDerpyCode <cdriza2@gmail.com>
2015-07-20 18:45:08 -0700

    Create cl_hitmarker.lua

---
+++ Hitmarkers/lua/autorun/client/cl_hitmarker.lua
@@ -0,0 +1,29 @@
+//resource.AddFile("sound/hitmarker.mp3")
+
+local alpha = 255
+
+local function PaintHitmarker()
+	if 0 >= alpha then
+		alpha = 255
+		hook.Remove("HUDPaint", "Hitmarker")
+	end
+
+	local x = ScrW() / 2
+	local y = ScrH() / 2
+	
+	surface.SetDrawColor(Color(255, 255, 255, alpha))
+
+	surface.DrawLine(x - 15, y - 15, x - 5, y - 5)
+	surface.DrawLine(x - 15, y + 15, x - 5, y + 5)
+	surface.DrawLine(x + 15, y - 15, x + 5, y - 5)
+	surface.DrawLine(x + 15, y + 15, x + 5, y + 5)
+	alpha = alpha - 5
+end
+
+local function Hitmarker()
+	//LocalPlayer():EmitSound("hitmarker.mp3", 130)
+	alpha = 255
+	hook.Add("HUDPaint", "Hitmarker", PaintHitmarker)
+end
+
+net.Receive("Hitmarker", Hitmarker)
